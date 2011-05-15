9d38ae8fbe5993d1d86737450362711dc99e2ff1
OK <ok100.ok100.ok100@gmail.com>
2011-05-15 12:02:50 +0200

    simplified by using acpi

--- Scripts/battery_dzen.sh
+++ Scripts/battery_dzen.sh
@@ -6,20 +6,15 @@
 #
 
 AC=$(cat /sys/class/power_supply/AC0/online)
-STATEFILE='/proc/acpi/battery/BAT0/state'
-INFOFILE='/proc/acpi/battery/BAT0/info'
+STATE=$(acpi -b | cut -d " " -f4 | cut -d "%" -f1)
 LOWBAT=35
 CRITBAT=15
-BAT_FULL=`cat $INFOFILE|grep design|line|cut -d " " -f 11`
-RCAP=`cat $STATEFILE|grep remaining|cut -d " " -f 8`
-RPERCT=`expr $RCAP \* 100`
-RPERC=`expr $RPERCT / $BAT_FULL`
 
 if [ "$AC" = "1" ]; then
 	echo '^i(/home/ok/.dzen/icons/ac_01.xbm) ^fg(\#477AB3)AC^fg()'
 else
-	if [ $RPERC -le $LOWBAT ]; then
-		if [ $RPERC -le $CRITBAT ]; then
+	if [ $STATE -le $LOWBAT ]; then
+		if [ $STATE -le $CRITBAT ]; then
 			echo '^fg(\#BF4D80)^i(/home/ok/.dzen/icons/bat_empty_02.xbm) ${battery_percent}^fg()'
 		else
 			echo '^fg(\#A270A3)^i(/home/ok/.dzen/icons/bat_low_02.xbm) ${battery_percent}^fg()'

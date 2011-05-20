ce3373a87c2fe5e2532460adbcb92d5ea05a5cb5
OK <ok100.ok100.ok100@gmail.com>
2011-05-20 18:58:32 +0200

    remove icons from status bar

--- Scripts/battery_dzen.sh
+++ Scripts/battery_dzen.sh
@@ -11,15 +11,15 @@ LOWBAT=35
 CRITBAT=15
 
 if [ "$AC" = "1" ]; then
-	echo '^i(/home/ok/.dzen/icons/ac_01.xbm) ^fg(\#477AB3)AC^fg()'
+	echo '^fg(\#477AB3)AC^fg()'
 else
 	if [ $STATE -le $LOWBAT ]; then
 		if [ $STATE -le $CRITBAT ]; then
-			echo '^fg(\#BF4D80)^i(/home/ok/.dzen/icons/bat_empty_02.xbm) ${battery_percent}^fg()'
+			echo '^fg(\#BF4D80)${battery_percent}%^fg()'
 		else
-			echo '^fg(\#A270A3)^i(/home/ok/.dzen/icons/bat_low_02.xbm) ${battery_percent}^fg()'
+			echo '^fg(\#A270A3)${battery_percent}%^fg()'
 		fi
 	else
-		echo '^i(/home/ok/.dzen/icons/bat_full_02.xbm) ^fg(\#519C7D)${battery_percent}^fg()'
+		echo '^fg(\#519C7D)${battery_percent}%^fg()'
 	fi
 fi

aae68aa086fabd78b2706fda74d209110fb9b108
OK <ok100.ok100.ok100@gmail.com>
2011-06-21 20:53:36 +0200

    change default foreground color

--- Scripts/dwm-reload.sh
+++ Scripts/dwm-reload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 killall dzen2 fbpanel dwm
-sleep 1 && conky -c ~/.conkyrc_dzen | dzen2 -e 'button3=' -p -fn 'arial:bold:size=9' -h 16 -bg '#121212' -fg '#696969' -expand left &
+sleep 1 && conky -c ~/.conkyrc_dzen | dzen2 -e 'button3=' -p -fn 'terminus:size=9' -h 14 -bg '#121212' -fg '#5E5E5E' -expand left &
 sleep 2 && fbpanel &

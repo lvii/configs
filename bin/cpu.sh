fad982f3008d6a947a5ef625dec6baeb0d044681
OK <ok100.ok100.ok100@gmail.com>
2011-11-25 12:51:56 +0100

    updated

--- bin/cpu.sh
+++ bin/cpu.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-#
-## cpu.sh
-## author  : OK <ok100.ok100.ok100@gmail.com>
-## website : https://github.com/ok100
-#
 
-CPU_FREQ=`cat /proc/eee/fsb | cut -d " " -f1`
-if [ $CPU_FREQ = 100 ]; then
-    echo "^fg(\#B3354C)"
-fi
-if [ $CPU_FREQ = 85 ]; then
-    echo "^fg(\#A64286)"
-fi
-if [ $CPU_FREQ = 70 ]; then
-    echo "^fg(\#337373)"
-fi
+let CPU_FREQ=`cat /proc/eee/fsb | cut -d " " -f1`*9
+echo $CPU_FREQ
--- bin/cpu_control.sh
+++ bin/cpu_control.sh
@@ -14,11 +14,11 @@ if [ "$FSB" = "70 24 0" ]; then
           icon=null\n
           \n
           item=Medium\n
-		  cmd=sudo /home/ok/bin/eeecpufreq -f 765\n
+		  cmd=sudo /home/ok/bin/eeecpufreq -f 729\n
           icon=null"
 fi
 
-if [ "$FSB" = "85 24 0" ]; then
+if [ "$FSB" = "81 24 0" ]; then
     MENU="item=High\n
 		  cmd=sudo /home/ok/bin/eeecpufreq -f 900\n
           icon=null\n
@@ -30,7 +30,7 @@ fi
 
 if [ "$FSB" = "100 24 1" ]; then
     MENU="item=Medium\n
-		  cmd=sudo /home/ok/bin/eeecpufreq -f 765\n
+		  cmd=sudo /home/ok/bin/eeecpufreq -f 729\n
           icon=null\n
           \n
           item=Low\n

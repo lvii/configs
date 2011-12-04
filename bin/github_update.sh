814da1a513e324d7fcb68edeb6b3facd46aad47d
OK <ok100.ok100.ok100@gmail.com>
2011-12-04 21:32:40 +0100

    updated

--- bin/github_update.sh
+++ bin/github_update.sh
@@ -4,7 +4,7 @@ PROJECT_DIR=~/build
 
 old_dir=`pwd`
 for dir in $PROJECT_DIR/*; do
-    if [ -d $dir/.git ]; then 
+    if [ -d "$dir/.git" ]; then 
         cd $dir
         if [ "`git status | grep 'Your branch is ahead of'`" != "" ]; then
             git push -u origin master

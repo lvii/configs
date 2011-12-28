80ab0bb813251f34d9c03fe5f5a8cc10634a85c9
OK <ok100.ok100.ok100@gmail.com>
2011-12-28 18:13:52 +0100

    updated

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
-#
-## Eee PC battery monitor
-## author  : OK <ok100.ok100.ok100@gmail.com>
-## website : https://github.com/ok100
-#
 
-MAX_mV=7543
+MAX_mV=7900
 MIN_mV=6171
 CRITBAT=20
 
-AC=`cat /sys/class/power_supply/AC0/online`
 CUR_mV=`cat /proc/acpi/battery/BAT0/state | grep mV | cut -d " " -f11`
 
-if [ "$AC" = "1" ]; then
-	echo "¡ ^fg(\#808080)\${battery_percent}%"
+let DIVIDER=($MAX_mV-$MIN_mV)/100
+let STATE=($CUR_mV-$MIN_mV)/$DIVIDER
+
+if [ $STATE -gt 100 ]; then STATE=100; fi
+if [ $STATE -lt 0 ]; then STATE=0; fi
+
+if [ $STATE -lt $CRITBAT ]; then
+	echo "î ^fg(\#B3354C)$STATE%"
 else
-	let DIVIDER=($MAX_mV-$MIN_mV)/100
-	let STATE=($CUR_mV-$MIN_mV)/$DIVIDER
-
-	if [ $STATE -gt 100 ]; then STATE=100; fi
-	if [ $STATE -lt 0 ]; then STATE=0; fi
-
-	if [ $STATE -lt $CRITBAT ]; then
-		echo "î ^fg(\#B3354C)$STATE%"
-	else
-		echo "ð ^fg(\#808080)$STATE%"
-	fi
+	echo "ð ^fg(\#808080)$STATE%"
 fi
-
